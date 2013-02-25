af56ffc7a0c55f412f2d8d1ef066a5775276be31
Jesse Jurman <j.r.jurman@gmail.com>
2013-02-25 15:40:46 -0500

    started anagrammer

---
+++ Anagrammer/anagrammer.rb
@@ -0,0 +1,12 @@
+#anagrammer.rb by Jesse Jurman
+#anagram solver
+
+# takes in a dictionary file and a block to check lines with
+# returns a list of valid words, reverse ordered by size (largest first)
+def loadDict(dictFile, func)
+end
+
+# takes in a filename and a string of letters (the anagram)
+# returns the largest valid word from the anagram
+def solve(dictFile, anagram)
+end
