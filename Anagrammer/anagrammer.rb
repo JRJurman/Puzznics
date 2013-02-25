cf7ed7914416fe3fcdc73579f9d6d5a15ed86d25
Jesse Jurman <j.r.jurman@gmail.com>
2013-02-25 17:49:30 -0500

    solves anagrams, a bit slow, 13 lines

--- Anagrammer/anagrammer.rb
+++ Anagrammer/anagrammer.rb
@@ -1,12 +1,13 @@
 #anagrammer.rb by Jesse Jurman
-#anagram solver
-
-# takes in a dictionary file and a block to check lines with
-# returns a list of valid words, reverse ordered by size (largest first)
-def loadDict(dictFile, func)
-end
-
-# takes in a filename and a string of letters (the anagram)
-# returns the largest valid word from the anagram
-def solve(dictFile, anagram)
-end
+def parse(word, char='') #form a regex parsable version of the word
+  word.chomp.upcase.split('').sort.join(char) end
+def loadDict(dictFile, reg) # takes in a dictionary file and a block to check lines with
+  res = []
+  File.open(dictFile).each_line { |word| res += [word.chomp] if reg.match(parse(word)) }
+  res.sort_by {|x| -x.size }
+end # returns a list of valid words, reverse ordered by size (largest first)
+def solve(dictFile, anagram) # takes in a filename and a string of letters (the anagram)
+  filter = /^#{anagram.upcase.split('').sort.join('?')}?$/
+  puts filter
+  res = loadDict(dictFile, filter )
+end # returns the largest valid word from the anagram
